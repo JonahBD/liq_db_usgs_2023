066a0937ce7d3a0b4de84a7e4ee13e4b8cf6a380
JonahBD <jonahsipod5@gmail.com>
2023-10-26 17:00:28 -0600

    started ML input code

--- ML_functions.py
+++ ML_functions.py
@@ -16,40 +16,43 @@ def finding_max_depth (input_folder_path_calculated_files):
 
         site_list.append(site)
 
-
     max_depth = max(max_depth_list)
     max_depth_index = max_depth_list.index(max_depth)
-    site_name_max_depth = site_list[max_depth_index]
+    max_depth_site_name = site_list[max_depth_index]
 
+    return max_depth, max_depth_site_name
 
-    return max_depth, site_name_max_depth
-
-def create_monster_df (input_folder_path_calculated_files, depth_column_name):
+def create_monster_df (input_folder_path, depth_column_name, max_depth, depth_step):
 
     counter = 0
     monster_df = pd.DataFrame()
+    monster_depth = depth_step
 
-    for filename in glob.glob(os.path.join(input_folder_path_calculated_files, "*.xls*")):
-        site = os.path.basename(filename).rstrip(".xls")
-        df = pd.read_excel(filename)
-        print(site)
+    df = pd.read_excel(glob.glob(os.path.join(input_folder_path, "*.xls*"))[0])
+    # for column in df.columns:
+        # print(column, df.columns.get_loc(column))
 
-        temp_col_names = []
-        temp_col_vals = []
-
-        for index, row in df.iterrows():
-            depth = row["Depth (m)"]
-            for column in df.columns:
-                if column ==  depth_column_name:
-                    continue
-                else:
-                    var = row[column]
-                    # monster_df.at[counter , column + "_d_" + str(depth)] = row[column]
-                    temp_col_names.append(column + "_d_" + str(depth))
-                    temp_col_vals.append(row[column])
-        # df_col_names = pd.DataFrame(temp_col_names)
-        # df_col_val = pd.DataFrame(temp_col_vals)
-        monster_df = pd.DataFrame({"header": temp_col_names, "vals": temp_col_vals}).T
-        # monster_df.loc[counter] = {temp_col_vals}
-        counter += 1
-    return monster_df
+    # for filename in glob.glob(os.path.join(input_folder_path, "*.xls*")):
+    #     site = os.path.basename(filename).rstrip(".xls")
+    #     df = pd.read_excel(filename)
+    #     print(site)
+    #
+    #     temp_col_names = []
+    #     temp_col_vals = []
+    #
+    #     for index, row in df.iterrows():
+    #         depth = row[depth_column_name]
+    #         for column in df.columns:
+    #             if column ==  depth_column_name:
+    #                 continue
+    #             else:
+    #                 # monster_df.at[counter , column + "_d_" + str(depth)] = row[column]
+    #                 temp_col_names.append(column + "_d_" + str(monster_depth))
+    #                 temp_col_vals.append(row[column])
+    #     # df_col_names = pd.DataFrame(temp_col_names)
+    #     # df_col_val = pd.DataFrame(temp_col_vals)
+    #     monster_df = pd.DataFrame({"header": temp_col_names, "vals": temp_col_vals}).T
+    #     # monster_df.loc[counter] = {temp_col_vals}
+    #     counter += 1
+    #     monster_depth += depth_step
+    # return monster_df
--- ML_main.py
+++ ML_main.py
@@ -5,14 +5,24 @@ import glob, os
 from datetime import datetime
 
 ################ USER INPUTS ############################
-input_folder_path_calculated_files = r"C:\Users\hf233\Documents\Italy\5. CPTU standard\calculated files\testing files"
+input_folder_path = r"C:\Users\jdundas2\Documents\Step 5 downloads\testing_finished"
 export_folder_path = r"C:\Users\hf233\Documents\Italy\5. CPTU standard\Files from drive\ran tests"
+depth_column_name = "Depth (m)"
+depth_step = 0.01 # in meters
 #########################################################
 
-# max_depth, site_of_max_depth  = finding_max_depth(input_folder_path_calculated_files)
-# print(max_depth, site_of_max_depth)
+# max_depth, max_depth_site = finding_max_depth(input_folder_path)
+# print(max_depth, max_depth_site)
+max_depth = 'you mom'
 
-df = create_monster_df(input_folder_path_calculated_files, "Depth (m)")
+df = pd.read_excel(glob.glob(os.path.join(input_folder_path, "*.xls*"))[0])
+for column in df.columns:
+    print(column, df.columns.get_loc(column))
+
+columns = int(input('What columns do you want to include: '))
+print(columns, type(columns))
+
+df = create_monster_df(input_folder_path, depth_column_name, max_depth, depth_step)
 
 
 
